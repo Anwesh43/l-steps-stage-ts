334801478203743fbcb785d3a66d1bc7a94f4b69
Anwesh43 <anweshthecool0@gmail.com>
2019-06-07 17:58:31 +0530

    adding ScaleUtil class

--- LStepsStage.ts
+++ LStepsStage.ts
@@ -6,6 +6,31 @@ const strokeFactor : number = 90
 const sizeFactor : number = 2.9
 const foreColor : string = "#0D47A1"
 const backColor : string = "#BDBDBD"
+const nodes : number = 5
+const lines : number = 4
+
+class ScaleUtil {
+
+    static maxValue(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxValue(scale, i, n)) * n
+    }
+
+    static scaleFactor(scale : number) : number {
+        return Math.floor(scale / scDiv)
+    }
+
+    static mirrorValue(scale : number, a : number, b : number) : number {
+        const k : number = ScaleUtil.scaleFactor(scale)
+        return (1 - k) / a + k / b
+    }
+
+    static updateValue(scale : number, dir : number, a : number, b : number) {
+        return ScaleUtil.mirrorValue(scale, a, b) * dir * scGap
+    }
+}
 
 class LStepsStage {
 
